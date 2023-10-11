c720eeeccbf0c0018453d607be546c7815396cf6
fehkelvink@gmail.com <fehkelvink@gmail.com>
2023-10-11 22:34:48 +0100

    Testing add bank update flow

--- js/bank-update.js
+++ js/bank-update.js
@@ -1,35 +1,26 @@
+//confirm details page
 function openPageTwo() {
-  window.location.href = '../doc/add-bank.html';
+  window.open('../doc/add-bank.html', '_blank');
 }
 
-function updatePageOne() {
+//add bank page
+function saveAndClose() {
+  const lastDigits = document.getElementById('accNumberInput').value;
   const bankName = document.getElementById('bankNameInput').value;
-  const accNumberInput = document.getElementById('accNumberInput').value;
 
-  // Extract last four digits
-  const lastFourDigits = accNumberInput.slice(-4);
-
-  // Store the values in localStorage
-  localStorage.setItem('bankName', bankName);
-  localStorage.setItem('lastFourDigits', lastFourDigits);
-  
-  window.location.href = '../doc/confirm-details.html';
-}
-
-// Retrieve and update values from localStorage on page load
-window.onload = function() {
-  const storedBankName = localStorage.getItem('bankName');
-  const storedLastFourDigits = localStorage.getItem('lastFourDigits');
-
-  if (storedBankName && storedLastFourDigits) {
-    const bankNameElement = document.querySelector('.bank-name p');
-    const lastDigitsElement = document.getElementById('lastDigits');
-
-    bankNameElement.innerText = storedBankName;
-    lastDigitsElement.innerText = storedLastFourDigits;
+  // Send the values back to Page One and close this window
+  const openerWindow = window.opener;
+  if (openerWindow) {
+    const accNumberP = openerWindow.document.querySelector('.acc-number p:last-child');
+    const bankNameP = openerWindow.document.querySelector('.bank-name p');
+        
+    if (accNumberP) {
+      accNumberP.innerText = lastDigits;
+    }
+    if (bankNameP) {
+      bankNameP.innerText = bankName;
+    }
   }
 
-  // Clear the stored values from localStorage
-  localStorage.removeItem('bankName');
-  localStorage.removeItem('lastFourDigits');
-};
+  window.close();
+}

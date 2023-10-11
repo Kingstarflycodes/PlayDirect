0fb6523a45d9ba6d305c3e22cff1deb2dd8c1680
fehkelvink@gmail.com <fehkelvink@gmail.com>
2023-10-11 22:40:44 +0100

    updating last four digits

--- js/bank-update.js
+++ js/bank-update.js
@@ -1,21 +1,23 @@
-//confirm details page
+//confirm-details page
 function openPageTwo() {
   window.open('../doc/add-bank.html', '_blank');
 }
 
-//add bank page
+//add-bank page
 function saveAndClose() {
   const lastDigits = document.getElementById('accNumberInput').value;
+  const lastFourDigits = lastDigits.slice(-4);  // Extract the last 4 digits
+
   const bankName = document.getElementById('bankNameInput').value;
 
-  // Send the values back to Page One and close this window
+  // Send the values back to Comfirm-details page Page and close this window
   const openerWindow = window.opener;
   if (openerWindow) {
     const accNumberP = openerWindow.document.querySelector('.acc-number p:last-child');
     const bankNameP = openerWindow.document.querySelector('.bank-name p');
-        
+
     if (accNumberP) {
-      accNumberP.innerText = lastDigits;
+      accNumberP.innerText = lastFourDigits;
     }
     if (bankNameP) {
       bankNameP.innerText = bankName;
